d84c6c7a6f96c3cfb59254b2ab36ff2b99b1e8b1
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-05-03 15:39:12 -0400

    add images to carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,12 @@ export const Home = () => {
   const carouselItems = [
     { name: "blarg 1", imagePath: "slider-1.png", description: "First slide" },
     { name: "blarg 2", imagePath: "slider-2.png", description: "Second slide" },
+    { name: "blarg 2", imagePath: "slider-3.png", description: "Second slide" },
+    { name: "blarg 2", imagePath: "slider-4.png", description: "Second slide" },
+    { name: "blarg 2", imagePath: "slider-5.png", description: "Second slide" },
+    { name: "blarg 2", imagePath: "slider-6.png", description: "Second slide" },
+    { name: "blarg 2", imagePath: "slider-7.png", description: "Second slide" },
+
   ];
   return (
       <div>
