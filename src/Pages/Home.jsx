5f1e0f2e3090f352689053aaae4cef663f11ac9e
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-05-03 14:55:10 -0400

    test

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "../Components/Carousel";
 
 export const Home = () => {
   const carouselItems = [
-    { name: "blarg 1", imagePath: "slider-1.png", description: "First slide" },
+    { name: "blarg 1", imagePath: "katie_logo.png", description: "First slide" },
     { name: "blarg 2", imagePath: "slider-2.png", description: "Second slide" },
   ];
   return (
