c2b32d1c5699075e7d23029d44b14c384ac7302f
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-05-03 15:04:45 -0400

    add default element using router

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={< Home />} />
+
       </Routes>
       <Footer/>
     </BrowserRouter>
