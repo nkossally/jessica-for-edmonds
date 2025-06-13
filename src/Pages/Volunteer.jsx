05d0f9b139635a7009d213ec1154f52bbd1d27f4
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-06-13 01:39:16 -0400

    css

--- src/Pages/Volunteer.jsx
+++ src/Pages/Volunteer.jsx
@@ -2,12 +2,14 @@ import { SignupForm } from "../Components/SignupForm";
 export const Volunteer = () => {
   return (
     <div className="volunteer-container">
-        <h1 className="volunteer-title">Join Zohran for NYC</h1>
-        <div className="volunteer-text">
-            This campaign is about talking to thousands of New Yorkers across the city about how we make this city work better for the many and not the few. And to do this, we need you! Sign up below, or check out events near you.
-
-        </div>
-      <SignupForm />
-    </div>
+      <h1 className="volunteer-title">Join Zohran for NYC</h1>
+      <div className="volunteer-text">
+        This campaign is about talking to thousands of New Yorkers across the
+        city about how we make this city work better for the many and not the
+        few. And to do this, we need you! Sign up below, or check out events
+        near you.
+      </div>
+        <SignupForm />
+    </div> 
   );
 };
