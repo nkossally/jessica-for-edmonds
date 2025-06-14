23f6ea01103d22978a83af9ed8d3c131f77f76b7
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-06-14 09:42:41 -0400

    css

--- src/Pages/DonatePage.jsx
+++ src/Pages/DonatePage.jsx
@@ -2,9 +2,12 @@ import { DonateForm } from "../Components/DonateForm";
 import { NavBar } from "../Components/NavBar";
 export const DonatePage = () => {
   return (
-    <div>
+    <div className="donate-container">
+      <div className="donate-title">
+        Donate to Jessica For Edmonds
+      </div>
+  
       <DonateForm />
-
     </div>
   );
 };
