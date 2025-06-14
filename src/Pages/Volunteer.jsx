f87909b3ac401649fa6b06531fe27ad9971dc382
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-06-14 09:53:41 -0400

    css

--- src/Pages/Volunteer.jsx
+++ src/Pages/Volunteer.jsx
@@ -2,12 +2,9 @@ import { SignupForm } from "../Components/SignupForm";
 export const Volunteer = () => {
   return (
     <div className="volunteer-container">
-      <h1 className="volunteer-title">Join Zohran for NYC</h1>
+      <div className="volunteer-title">Join Jessica for Edmonds</div>
       <div className="volunteer-text">
-        This campaign is about talking to thousands of New Yorkers across the
-        city about how we make this city work better for the many and not the
-        few. And to do this, we need you! Sign up below, or check out events
-        near you.
+        This campaign is about ...
       </div>
         <SignupForm />
     </div> 
