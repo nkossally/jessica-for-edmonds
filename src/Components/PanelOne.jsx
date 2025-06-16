bd65e031b86d080629f2f65269ebd0f4ab22f621
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-06-16 19:58:41 -0400

    css

--- src/Components/PanelOne.jsx
+++ src/Components/PanelOne.jsx
@@ -3,31 +3,20 @@ import classNames from "classnames";
 export const PanelOne = () => {
   return (
     <div className={classNames("panel")}>
-      <div className="inner-panel">
-        <div className="panel-title"> why I’m running</div>
+      <div className="half-panel">
+        <div className="panel-title"> Lorem ipsum dolor</div>
         <div className="panel-text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac erat
           sed enim tincidunt tempor. Quisque at accumsan ex. Nulla tincidunt
-          massa ligula, sed egestas erat mattis eu. Vivamus dignissim lectus
-          nibh, quis euismod sem pretium nec. Sed molestie vel tortor et
-          rhoncus. Nullam ut arcu ut libero congue tempus. Maecenas pharetra eu
-          nisl sed lobortis. Cras iaculis orci purus, a sollicitudin elit
-          iaculis eget. Sed ultricies rhoncus tempus. In et ullamcorper risus,
-          at vulputate nulla. Cras aliquet orci nec mauris scelerisque, ut
-          imperdiet felis molestie. Curabitur elementum neque lectus. Donec at
-          tellus eros. Proin non feugiat lectus. Mauris rutrum rhoncus
-          fermentum. Duis mi dolor, sollicitudin in mi nec, laoreet volutpat
-          dolor.
+          massa ligula
         </div>
         <div className="panel-text">
           Nunc finibus finibus malesuada. Nullam non ornare turpis. In iaculis,
           leo ac sagittis rhoncus, felis tellus efficitur mauris, eget faucibus
-          dolor tellus sit amet nunc. Suspendisse sit amet finibus mauris.
-          Quisque sollicitudin nulla quis fringilla maximus. Nullam diam magna,
-          efficitur lacinia pellentesque sit amet, tempor in nisl. Sed faucibus
-          aliquet neque varius sagittis. Nullam ac varius velit, a ornare nisl.
+          dolor tellus sit amet nunc.
         </div>
       </div>
+      <div className="half-panel"></div>
     </div>
   );
 };
--- src/Components/PanelTwo.jsx
+++ src/Components/PanelTwo.jsx
@@ -2,10 +2,21 @@ import classNames from "classnames";
 
 export const PanelTwo = () => {
   return (
-    <div className={classNames("panel", "panel-two")}>
-      <div className="inner-panel">
-        <div className="panel-title"> Lorem ipsum dolor sit amet</div>
-        <div className="panel-quote">“My campaign is for...</div>
+  <div className={classNames("panel", "panel-two")}>
+       <div className="half-panel"></div>
+
+      <div className="half-panel">
+        <div className="panel-title"> Lorem ipsum dolor</div>
+        <div className="panel-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac erat
+          sed enim tincidunt tempor. Quisque at accumsan ex. Nulla tincidunt
+          massa ligula
+        </div>
+        <div className="panel-text">
+          Nunc finibus finibus malesuada. Nullam non ornare turpis. In iaculis,
+          leo ac sagittis rhoncus, felis tellus efficitur mauris, eget faucibus
+          dolor tellus sit amet nunc.
+        </div>
       </div>
     </div>
   );
