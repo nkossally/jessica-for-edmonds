e449c527b538e591ac4130f231e7df39a9c52c35
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-06-16 19:42:37 -0400

    css

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,22 +1,28 @@
 export const HeroSection = () => {
   return (
     <div className="hero-container">
+      <div className="hero-inner-container">
       <div className="hero-row-one">
         <div className="red-name">Jessica</div>
         <div className="for">for</div>
         <div className="red-name">Edmonds</div>
       </div>
-      <div>City Council Pos. 2</div>
-      <div>Jessica Bachman</div>
-      <div className="">is running for City Council to</div>
+      <div className="hero-row-two">City Council Pos. 2</div>
+      <img
+        className="wave"
+        src={process.env.PUBLIC_URL + "/" +"wave_blue.svg"}
+      />
+      <div className="hero-name">Jessica Bachman</div>
+      <div className="hero-text">is running for City Council to</div>
 
-      <div className="">lower the cost of living for</div>
+      <div className="hero-text">lower the cost of living for</div>
 
-      <div className="">working class Edmonders.</div>
+      <div className="hero-text">working class Edmonders.</div>
       <img
         className="hero-image"
-        src={process.env.PUBLIC_URL + "/" + "jessica 14.png"}
+        src={process.env.PUBLIC_URL + "/" + "Jessica_Bachman.png"}
       />
+    </div>
     </div>
   );
 };
